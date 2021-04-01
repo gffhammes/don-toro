d299eb4c03d072fdda047fd661ca36d5b52fcd37
Guilherme Hammes <gffhammes@gmail.com>
2021-04-01 17:26:16 -0300

    20210401-03

--- files/style/script.js
+++ files/style/script.js
@@ -83,6 +83,38 @@ $('.slider-home-loja').slick({
         }
     ]
 });
+$('.slider-home-parceiros').slick({
+    initialSlide:0,
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:true,
+    responsive: [
+        {
+            breakpoint: 996,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            }
+        },
+        {            
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {            
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
 $('.slider-home-eventos').slick({
     initialSlide:0,
     dots: true,
