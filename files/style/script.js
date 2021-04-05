a3610738916145d217fb626587263b7ebcda667c
Guilherme Hammes <gffhammes@gmail.com>
2021-04-05 09:42:43 -0300

    20210405-1

--- files/style/script.js
+++ files/style/script.js
@@ -78,7 +78,7 @@ $('.slider-home-loja').slick({
           breakpoint: 1024,
           settings: {
             slidesToShow: 1,
-            slidesToScroll: 1,
+            slidesToScroll: 1
           }
         }
     ]
@@ -96,21 +96,21 @@ $('.slider-home-parceiros').slick({
             breakpoint: 996,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 1,
+              slidesToScroll: 1
             }
         },
         {            
             breakpoint: 767,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 1,
+                slidesToScroll: 1
             }
         },
         {            
             breakpoint: 575,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1,
+                slidesToScroll: 1
             }
         }
     ]
@@ -133,4 +133,24 @@ $('.slider-carnes-kit').slick({
     slidesToScroll: 1,
     arrows:false,
     mobileFirst: true,
+    responsive: [
+        {            
+            breakpoint: 1199,
+            settings: "unslick"
+        },
+        {            
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {            
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
 });
